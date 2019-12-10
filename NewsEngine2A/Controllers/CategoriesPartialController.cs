55c102beae635d0e395f85a3e7bf09d0cafa71b1
Nicolae Adrian-Danut <adrian.danut.nicolae@gmail.com>
2019-12-10 12:57:11 +0200

    Created a partialView for categories to be included in navbar(_layout)

---
+++ NewsEngine2A/Controllers/CategoriesPartialController.cs
@@ -0,0 +1,17 @@
+﻿using NewsEngine2A.Context;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace NewsEngine2A.Controllers
+{
+    public class CategoriesPartialController : Controller
+    {
+        private readonly NewsEngineContext _context = new NewsEngineContext();
+        public ActionResult NavCategories()
+        {
+            var categories = _context.NewsCategories.ToList();
+
+            return PartialView(categories);
+        }
+    }
+}
